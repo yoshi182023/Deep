7e0344b1d91e57c3faa5a18ccece321bb6acc2fd
Gong <125266206+yoshi182023@users.noreply.github.com>
2026-06-24 21:16:14 +0000

    设置好环境

---
+++ deep_atlas_mli 2/environment_setup/name_generator.py
@@ -0,0 +1,22 @@
+def get_name():
+    import random
+
+    names = [
+        "Alex",
+        "Avery",
+        "Casey",
+        "Charlie",
+        "Dakota",
+        "Hayden",
+        "Jamie",
+        "Jordan",
+        "Morgan",
+        "Phoenix",
+        "Riley",
+        "Taylor",
+    ]
+
+    # To get random names, comment out this line
+    names = ["person who loaded the name_generator.py custom module file, but hasn't edited it yet"]
+
+    return random.choice(names)
